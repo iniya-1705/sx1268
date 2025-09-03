b7709c92f2995ebb9b1dc7ac8dff434b9153184e
iniya-1705 <iniyaarivazhagan@gmail.com>
2025-09-03 11:33:54 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,20 +2,28 @@
 import time
 from LoRaRF import SX126x
 
+# Initialize LoRa
 lora = SX126x()
-lora.begin()
-lora.setFrequency(433000000)
-lora.setTxPower(22)
-lora.setLoRaModulation(7, 5, 125000)
-lora.setLoRaPacket(8, True, 255, False, True)
 
-print("LoRa RX started...")
+def setup():
+    print("LoRa Receiver Init...")
+    lora.begin()
+    lora.setFrequency(433000000)          # 433 MHz (must match TX)
+    lora.setRxGain(0x94, 0xF5)            # Recommended RX gain
+    lora.setLoRaModulation(7, 5, 125000)  # Same as TX
+    lora.setLoRaPacket(8, True, 255, False, True)
 
-while True:
+def loop():
     if lora.available():
-        data = []
-        while lora.available():
-            data.append(lora.read())
-        msg = "".join(chr(b) for b in data)
-        print("Received:", msg, "| RSSI:", lora.packetRssi())
-    time.sleep(0.1)
+        length = lora.available()
+        msg = ""
+        for i in range(length):
+            msg += chr(lora.read())
+        print("Received:", msg)
+
+    time.sleep(0.5)
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
