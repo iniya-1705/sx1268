073fe2556314e0700099d8c754ad434c1082df89
iniya-1705 <iniyaarivazhagan@gmail.com>
2025-09-03 11:33:27 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -2,20 +2,34 @@
 import time
 from LoRaRF import SX126x
 
+# Initialize LoRa
 lora = SX126x()
-lora.begin()
-lora.setFrequency(433000000)
-lora.setTxPower(22)
-lora.setLoRaModulation(7, 5, 125000)
-lora.setLoRaPacket(8, True, 255, False, True)
 
-print("LoRa TX started...")
+def setup():
+    print("LoRa Transmitter Init...")
+    lora.begin()
+    lora.setFrequency(433000000)          # 433 MHz
+    lora.setTxPower(22)                   # Power in dBm
+    lora.setLoRaModulation(7, 5, 125000)  # SF7, CR 4/5, BW 125kHz
+    lora.setLoRaPacket(8, True, 255, False, True)  # Preamble, CRC etc.
 
-while True:
-    msg = "Hello"
+def loop():
+    msg = "Hello from Pi"
     print("Sending:", msg)
     lora.beginPacket()
     for ch in msg:
-        lora.write(ord(ch))
+        lora.write(ord(ch))   # send char by char
     lora.endPacket()
+    
+    # confirm TX done
+    if lora.checkIrq("TX_DONE"):
+        print("Packet sent successfully")
+    else:
+        print("TX not confirmed")
+
     time.sleep(2)
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        loop()
